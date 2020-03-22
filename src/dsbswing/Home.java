67a0a625c31e78f0e175181fb6a6759b14323ef0
EMProducciones <emproducciones2257@gmail.com>
2020-03-22 18:41:14 -0300

    cambio desde aca

--- src/dsbswing/Home.java
+++ src/dsbswing/Home.java
@@ -480,7 +480,7 @@ public class Home extends javax.swing.JFrame {
     	
     	if(c!=null) {
     		
-    		borrar = temp.recuperarEquiposPorCampeonato(c, 401);
+    		borrar = temp.recuperarEquiposPorCampeonato(c, 100);
     		System.out.print("NOMBRE: " +borrar.getNombre());
     		System.out.print("NUM A: " +borrar.getNumA());
     		System.out.print("NUM B: " +borrar.getNumB());
