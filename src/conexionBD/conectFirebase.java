bcbcf8466c7e1e0c3c7692edbe3b04efd5d577b2
EMProducciones <emproducciones2257@gmail.com>
2020-05-05 10:30:18 -0300

    Arranque Firebase B

--- src/conexionBD/conectFirebase.java
+++ src/conexionBD/conectFirebase.java
@@ -20,7 +20,6 @@ public class conectFirebase {
 						  .build();
 			FirebaseApp.initializeApp(options);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
