fbece461fd51b766db2aef9acd488003499514c5
EMProducciones <emproducciones2257@gmail.com>
2020-07-06 08:52:32 -0300

    Modulo leer excel. 6-7

--- src/res/leerExcel.java
+++ src/res/leerExcel.java
@@ -1,47 +1,63 @@
 package res;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.util.Iterator;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import java.io.*;
+import java.util.ArrayList;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.*;
 
 public class leerExcel {
 	
-		   static XSSFRow row;
-		   public static void main(String[] args) throws Exception {
-		      FileInputStream fis = new FileInputStream(new File("C:\\PERFUMERIA.xlsx"));
-		      
-		      try (XSSFWorkbook workbook = new XSSFWorkbook(fis)) {
-				XSSFSheet spreadsheet = workbook.getSheetAt(0);
-				  Iterator < Row >  rowIterator = spreadsheet.iterator();
-				  
-				  while (rowIterator.hasNext()) {
-				     row = (XSSFRow) rowIterator.next();
-				     Iterator < Cell >  cellIterator = row.cellIterator();
-				     
-				     while ( cellIterator.hasNext()) {
-				        Cell cell = cellIterator.next();
-				        
-				        switch (cell.getCellType()) {
-				           case NUMERIC:
-				              System.out.println(cell.getNumericCellValue() + " \t\t ");
-				              break;
-				           
-				           case STRING:
-				              System.out.println(
-				              cell.getStringCellValue() + " \t\t ");
-				              break;
-				        }
-				     }
-				     //System.out.println();
-				  }
+	static XSSFRow row;
+		public static void main(String[] args) throws Exception {
+			
+		FileInputStream fis = new FileInputStream(new File("C:\\PERFUMERIA.xls"));
+		
+		//creo libro 
+		Workbook workbook = WorkbookFactory.create(fis);
+		
+		//obtengo la primero hoja del libro
+		Sheet sheet = workbook.getSheetAt(0);
+		
+		ArrayList<Object> filasCelda;	
+	
+		for (Row row: sheet) {
+			filasCelda = new ArrayList<>();
+			
+            for(Cell cell: row) {
+            	
+            	if(cell.getCellType()==CellType.NUMERIC) {
+			    	filasCelda.add(cell.getNumericCellValue());
+            	}
+            	if (cell.getCellType()==CellType.STRING) {
+			    	filasCelda.add(cell.getStringCellValue());
+				}
+            }
+            procesarCelda(filasCelda);
+        }
+		
+		fis.close();
+		workbook.close();
+	}
+		private static void procesarCelda(ArrayList<Object> filasCelda) {
+			if(filasCelda.size()==3) {
+				
+				boolean estado = true;
+				char[] caracteres = filasCelda.get(0).toString().toCharArray();
+				
+				for (int i = 0; i < caracteres.length; i++) {
+					if (!Character.isDigit(caracteres[i])) {
+						estado=false;
+					}
+				}
+				
+				if(estado) {
+					System.out.println("CODIGO: " + filasCelda.get(0)+", DESC.: " + filasCelda.get(1) + " VALOR: $ " + filasCelda.get(2));
+				}	
+				
 			}
-		      fis.close();
+			
+		}	
 }
-}
+
 
 
